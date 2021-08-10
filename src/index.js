8f19db9a0a39167fae0823e5b8ce1ec324c48b5c
binepals <59314885+binepals@users.noreply.github.com>
2021-08-10 20:14:58 +0100

    Added constants and expressions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+
+const myName = "Binepal Singh";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {myName}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
